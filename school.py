8f64523d80eabe37c11ec0e49b3e752b1ef3648f
Elizabeth Fawcett <elizabethfawcett47@gmail.com>
2019-11-01 15:31:32 -0500

    function add_student()

--- school.py
+++ school.py
@@ -1,1 +1,9 @@
 class School:
+    def __init__(self, name, roster={}):
+        self.name = name
+        self.roster = roster
+    def add_student(self, name, grade):
+        if grade in self.roster:
+            self.roster[grade].append(name)
+        else:
+            self.roster[grade] = [name]
